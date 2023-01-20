c0fb5b13813fef838847c12ff90ebebb3438d801
Aakanksha-H <123055596+Aakanksha-H@users.noreply.github.com>
2023-01-20 12:47:31 +0530

    Added_line_length_equality_using_compareTo

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,25 @@
 package com.bridgelabz.day3;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("WELCOME TO LINE COMPARISON SOLUTION");
+
+        double line1, line2;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter end of fist line = ");
+        line1 = sc.nextDouble();
+
+        System.out.print("Enter end of second line = ");
+        line2 = sc.nextDouble();
+
+        if (Double.compare(line1,line2) == 0) {
+            System.out.println("Lines have same length");
+        }
+        else if (Double.compare(line1,line2) < 0)
+            System.out.println("Line 1 is smaller than Line 2");
+        else
+            System.out.println("Line 1 is greater than Line 2");
     }
 }
